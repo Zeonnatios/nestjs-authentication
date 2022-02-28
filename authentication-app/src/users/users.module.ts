9bfddb5bdb9d9a1ff4d49e2a24e82c8c7e00c3a1
Matheus Antonio <matheusantonio232@gmail.com>
2022-02-28 16:48:46 -0300

    criando metodos de auth

--- authentication-app/src/users/users.module.ts
+++ authentication-app/src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from 'src/auth/auth.module';
-import { UsersSchema } from './schemas/schemas.schemas';
+import { UsersSchema } from './schemas/users.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
