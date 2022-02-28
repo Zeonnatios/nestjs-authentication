84abb98906d54c11a292a1510bc94ae00f7f23e2
Matheus Antonio <matheusantonio232@gmail.com>
2022-02-28 11:47:48 -0300

    importando mongooseModule

--- authentication-app/src/app.module.ts
+++ authentication-app/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [],
   providers: [],
 })
